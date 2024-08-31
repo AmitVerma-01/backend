ec2eb8bf74feeb0e3983861e0522aae3bf43573a
Amit Verma <amitvermaup57@gmail.com>
2024-09-01 02:01:25 +0530

    use mongoDB aggregate pipeline and implement getChannelProfile controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -345,6 +345,73 @@ const updateUserCoverImage = asyncHandler(async(req, res)=>{
 
 })
 
+const getChannelProfile = asyncHandler(async(req, res)=>{
+    const username = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(401, "Username is not valid")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match : {
+                username : username?.toLowerCase()
+            }
+        },{
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "channel",
+                as : "subscribers"
+            }
+        },{
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "subscriber",
+                as : "subscribedTo"
+            }
+        },{
+            $addFields : {
+                subscriberCount : {
+                    $size : "$subscribers"
+                },
+                subscribedToCount : {
+                    $size : "$subscribedTo"
+                },
+                isSubscribed : {
+                    $cond : {
+                        if : {$in : [req.user?._id, "$subscribers.subscriber"]} ,
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },{
+            $project : {
+                fullName : 1,
+                username : 1,
+                avatar : 1,
+                coverImage : 1,
+                email : 1,
+                isSubscribed : 1,
+                subscribedToCount : 1,
+                subscriberCount : 1,
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(400, "Channel does not exists.")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0],"Channel data recieved successfully")
+    )
+})
+
 export { registerUser,
          loginUser,
          logoutUser, 
@@ -353,5 +420,6 @@ export { registerUser,
          changeCurrentPassword,
          updateUserDetailes,
          updateUserAvatar,
-         updateUserCoverImage
+         updateUserCoverImage,
+         getChannelProfile
         }
