824e9bd9306c47b98ba8fefed6d0d5120a0156dd
Amit Verma <amitvermaup57@gmail.com>
2024-11-12 12:11:02 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
 // Routes imports
 import userRouter from "./routes/user.routes.js"
 import healthcheckRouter from "./routes/healthcheck.route.js"
